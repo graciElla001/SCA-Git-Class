600aa864b0a47871e3eede4282f2072b026e14cc
graciElla001 <onwuasoanyaijeoma1@gmail.com>
2026-03-18 16:40:30 +0100

    I added a bashscript thasks a question and prints the comprehensive answer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SCA-Git-Class
 The assignments and tasks for the git class from She Code Africa
+I am doing my best.
---
+++ user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This is a sample script
+read -p "What is your name?: " name
+read -p "How old are you?: " age
+
+echo "Hello there $name, you are $age years old right?"
